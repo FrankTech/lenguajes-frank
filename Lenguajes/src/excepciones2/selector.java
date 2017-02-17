c05e9d9b5581bab8580118ebfbbbbb11e3ac237b
T-102 <T-102@PC199>
2017-02-16 19:40:02 -0600

    clase 16/02/2017

---
+++ Lenguajes/src/excepciones2/selector.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones2;
+
+/**
+ *
+ * @author T-102
+ */
+class selector {
+
+    static void showOpenDialog(AplicacionLectora aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
